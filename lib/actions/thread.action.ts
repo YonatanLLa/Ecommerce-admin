f6f3ea5c8aeaf6f9a2932376e81391c389cf2a2d
yonatanlla <yonatanllanto174@gmail.com>
2023-08-26 23:27:32 -0500

    card_comentarios

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -1,38 +1,68 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
-import { connectToDB } from "../mongoose"; 
+import { connectToDB } from "../mongoose";
 
 interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
 
-export async function createThread( {text, author, communityId, path}: Params) {
-    
-    try {
-        connectToDB();
-        
-        const createThread = await Thread.create({
-            text,
-            author,
-            community: null,
-        });
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
 
-        await User.findByIdAndUpdate(author, {
-            $push: {
-                threads: createThread._id
-            }
-        })
+    const createThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
 
-        revalidatePath(path)
-        
-    } catch (error) {
-        
-    }
-    
+    await User.findByIdAndUpdate(author, {
+      $push: {
+        threads: createThread._id,
+      },
+    });
+
+    revalidatePath(path);
+  } catch (error) {}
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmout = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmout)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "Id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmout + posts.length;
+
+  return { posts, isNext };
 }
