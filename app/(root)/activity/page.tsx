d38b73d74b7fed1916ce6d8d5c97fb6ad4b7a519
yonatanlla <yonatanllanto174@gmail.com>
2023-08-28 18:46:42 -0500

    activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,22 @@
-const Page = () => {
-    return (
-      <section>
-          <h1 className="head-text mb-10">Activity</h1>
-      </section>
-    )
-  }
-  
-  export default Page
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  // getActivity
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return (
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+    </section>
+  );
+}
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -129,3 +129,19 @@ export async function fetchUsers({
     throw new Error(`failed to fetch users: ${error.message}`);
   }
 }
+export async function getActivity(userId: string) {
+    try {
+      connectToDB()
+
+      // find all thread created by the user
+
+      const userThreads = await Thread.find({author: userId})
+
+      const childThreadIds = userThreads.reduce((acc, userThread) => {
+        return acc.concat(userThread.children)
+      })
+    } catch (error: any) {
+      throw new Error(`faild to fetch activity: ${error.message}`);
+      
+    }
+}
